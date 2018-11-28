eb70c8fce7174ccbf2ef1d2228177c039bd96a3f
laurascotch <laura.scoccianti@gmail.com>
2018-11-28 11:50:41 +0100

    Aggiunti controlli e nuovi casi di test

--- invert_test/k_invert.js
+++ invert_test/k_invert.js
@@ -1,18 +1,22 @@
 function k_invert(a, k){
     
-    if(k<=0) return null;
+    if(!(Array.isArray(a))) return null;
+
+    if(k<0) return null;
 
     if(k>a.length) return null;
 
     for(i = 0; i<a.length; i++){
         if(a[i]<=0) return null;
+        if(isNaN(a[i])) return null;
+        if(!(Number.isInteger(a[i]))) return null; 
     }
     
     var b = a.splice(-k);
 
     var c = b.concat(a);
 
-    console.log(c);
+    //console.log(c);
     return c;
 
 }
--- invert_test/k_invert.test.js
+++ invert_test/k_invert.test.js
@@ -7,7 +7,7 @@ test('sposta tutto l\'array', () => {
 });
 
 test('non spostare nulla', () => {
-    expect(k_invert([1, 3, 2, 6, 22], 0)).toEqual(null);
+    expect(k_invert([1, 3, 2, 6, 22], 0)).toEqual([1, 3, 2, 6, 22]);
 });
 
 test('spostane 3', () => {
@@ -16,6 +16,10 @@ test('spostane 3', () => {
 
 test('cerca di spostarne troppi', () => {
     expect(k_invert([1, 3, 2, 6, 22], 6)).toEqual(null);
+});
+
+test('cerca di spostarne troppi', () => {
+    expect(k_invert([], 2)).toEqual(null);
 });
 
 var b = [1, 0, 2, 3]
@@ -34,4 +38,28 @@ var d = [1, 4, 2, 3]
 
 test('k non è un intero positivo', () => {
     expect(k_invert(d, -3)).toEqual(null);
+});
+
+var e = [1, 'a', 3, 4]
+
+test('l\'array ha almeno un non numero', () => {
+    expect(k_invert(e, 3)).toEqual(null);
+});
+
+var f = [1, 1.25, 3, 4]
+
+test('l\'array non è composto di soli interi', () => {
+    expect(k_invert(f, 3)).toEqual(null);
+});
+
+var g = 42
+
+test('l\'array non è un array', () => {
+    expect(k_invert(g, 3)).toEqual(null);
+});
+
+var h = []
+
+test('l\'array è vuoto', () => {
+    expect(k_invert(h, 0)).toEqual([]);
 });
